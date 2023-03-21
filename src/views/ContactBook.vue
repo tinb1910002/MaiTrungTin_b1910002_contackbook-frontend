007d979ff5101a6e5f9d0d310f28a0c58ed4cdd3
tinb1910002 <113885800+tinb1910002@users.noreply.github.com>
2023-03-21 16:45:23 +0700

    Tao trang cap nhat lien he

--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -2,41 +2,50 @@
     <div class="page row">
         <div class="col-md-10">
             <InputSearch v-model="searchText" />
-    </div>
-    <div class="mt-3 col-md-6">
-        <h4>
-            Danh bạ
-            <i class="fas fa-address-book"></i>
-        </h4>
-        <ContactList
-            v-if="filteredContactsCount > 0"
-            :contacts="filteredContacts"
-            v-model:activeIndex="activeIndex"
-        />
-        <p v-else>Không có liên hệ nào.</p>
-    
-        <div class="mt-3 row justify-content-around align-items-center">
-            <button class="btn btn-sm btn-primary" @click="refreshList()">
-                <i class="fas fa-redo"></i> Làm mới
-            </button>
-            <button class="btn btn-sm btn-success" @click="goToAddContact">
-                <i class="fas fa-plus"></i> Thêm mới
-            </button>
-            <button
-                class="btn btn-sm btn-danger"
-                @click="removeAllContacts"
-            >
-                <i class="fas fa-trash"></i> Xóa tất cả
-            </button>
         </div>
-    </div>
-    <div class="mt-3 col-md-6">
-        <div v-if="activeContact">
+        <div class="mt-3 col-md-6">
             <h4>
-                Chi tiết Liên hệ
-                <i class="fas fa-address-card"></i>
+                Danh bạ
+                <i class="fas fa-address-book"></i>
             </h4>
-            <ContactCard :contact="activeContact" />
+            <ContactList
+                v-if="filteredContactsCount > 0"
+                :contacts="filteredContacts"
+                v-model:activeIndex="activeIndex"
+            />
+            <p v-else>Không có liên hệ nào.</p>
+        
+            <div class="mt-3 row justify-content-around align-items-center">
+                <button class="btn btn-sm btn-primary" @click="refreshList()">
+                    <i class="fas fa-redo"></i> Làm mới
+                </button>
+                <button class="btn btn-sm btn-success" @click="goToAddContact">
+                    <i class="fas fa-plus"></i> Thêm mới
+                </button>
+                <button
+                    class="btn btn-sm btn-danger"
+                    @click="removeAllContacts"
+                >
+                    <i class="fas fa-trash"></i> Xóa tất cả
+                </button>
+            </div>
+        </div>
+        <div class="mt-3 col-md-6">
+            <div v-if="activeContact">
+                <h4>
+                    Chi tiết Liên hệ
+                    <i class="fas fa-address-card"></i>
+                </h4>
+                <ContactCard :contact="activeContact" />
+                <router-link
+                    :to="{
+                        name: 'contact.edit',
+                        params: { id: activeContact._id },
+                    }"
+                >
+                    <span class="mt-2 badge badge-warning">
+                        <i class="fas fa-edit"></i> Hiệu chỉnh</span>
+                </router-link>
             </div>
         </div>
     </div>
