ee619b4cc5a9aabe507ac48d423ed33f5c608ea7
tinb1910002 <113885800+tinb1910002@users.noreply.github.com>
2023-03-21 21:00:18 +0700

    Tao trang them mot lien he moi

--- src/router/index.js
+++ src/router/index.js
@@ -6,17 +6,22 @@ const routes = [
         name: "contactbook",
         component: ContactBook,
     },
-
     {
         path: "/:pathMatch(.*)*",
         name: "notfound",
         component: () => import("@/views/NotFound.vue"),
     },
-
     {
         path: "/contacts/:id",
         name: "contact.edit",
         component: () => import("@/views/ContactEdit.vue"),
+        props: true
+    },
+
+    {
+        path: "/contacts/add",
+        name: "contact.add",
+        component: () => import("@/views/ContactAdd.vue"),
         props: true
     },
 
---
+++ src/views/ContactAdd.vue
@@ -0,0 +1,38 @@
+<template>
+    <div v-if="contact" class="page">
+        <h4>Thêm Liên Hệ</h4>
+        <ContactForm
+            :contact="contact"
+            @submit:contact="uploadContact"
+        />
+        <p>{{ message }}</p>
+    </div>
+</template>
+<script>
+    import ContactForm from "@/components/ContactForm.vue";
+    import ContactService from "@/services/contact.service";
+    export default {
+            components: {
+                ContactForm,
+            },
+        data() {
+            return {
+                contact: {},
+                message: "",
+            };
+        },
+        methods: {
+            async uploadContact(data) {
+                try {
+                    await ContactService.create(data);
+                    this.message = "Liên hệ được thêm thành công.";
+                } catch (error) {
+                    console.log(error);
+                }
+            },
+        },
+
+    };
+</script>
+
+    
